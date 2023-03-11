91aae28c496ed12327b71c34bc1686221ab90b8a
Hemant soni <sonihemant4741@gmail.com>
2023-03-11 19:03:38 +0530

    Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Footer from "@/Components/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
+
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
